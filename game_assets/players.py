dd3aa02102beb8d0e514afb53d7e853c64a19b18
Robert Var <mail.robertvari@gmail.com>
2022-09-14 15:24:10 +0200

    Update

--- game_assets/players.py
+++ game_assets/players.py
@@ -1,3 +1,5 @@
+import random
+
 class Player_Base:
     def __init__(self):
         self._name = None
@@ -10,7 +12,23 @@ class Player_Base:
     def create(self):
         first_names = ["Brittney", "Curtis", "Lucas", "Chip", "Simon"]
         last_names = ["Moriah", "Tristin", "Troy", "Gale", "Lynn"]
+        self._name = f"{random.choice(first_names)} {random.choice(last_names)}"
 
+        self._credits = random.randint(10, 100)
+
+    @property
+    def full_name(self):
+        return self._name
+
+    @property
+    def credits(self):
+        return self._credits
+
+    def __str__(self) -> str:
+        return self._name
+
+    def __repr__(self) -> str:
+        return self._name
 
 class Player(Player_Base):
     pass
@@ -22,3 +40,4 @@ class AIPlayer(Player_Base):
 # testing players
 if __name__ == "__main__":
     ai_player = AIPlayer()
+    print(ai_player)
